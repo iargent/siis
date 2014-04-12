fc36c2e7a213b37e2ab0cf01f9dd817d6b6bc493
Iain Argent <iargent@samlearning.com>
2014-04-12 21:18:31 +0300

    Initial EduBase scrape achieved. Can now get URN for any school

--- build.sbt
+++ build.sbt
@@ -6,5 +6,9 @@ scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
 "com.typesafe.slick" %% "slick" % "2.0.0",
-"org.slf4j" % "slf4j-nop" % "1.6.4"
+"org.slf4j" % "slf4j-nop" % "1.6.4",
+"org.seleniumhq.webdriver" % "webdriver-selenium" % "0.9.7376",
+"org.seleniumhq.webdriver" % "webdriver-htmlunit" % "0.9.7376",
+"org.seleniumhq.selenium" % "selenium-java" % "2.41.0" % "test",
+"org.scalatest" % "scalatest_2.10" % "2.1.0" % "test"
 )
--- src/main/scala/siis/hello.scala
+++ src/main/scala/siis/hello.scala
@@ -1,11 +1,33 @@
 package siis
 
-import scala.io.Source
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.By
 
 object hello {
   def main(args: Array[String]) {
     println("Main starting")
-    val  x=scala.io.Source.fromURL("http://www.google.com")
-    println(x.getLines().mkString("\n"))
+
+    val url = "http://www.education.gov.uk/edubase/public/quickSearchResult.xhtml?myListCount=0"
+
+    val driver = new HtmlUnitDriver
+    driver.get(url)
+    val raw = driver.findElementsByXPath("//*[@class=\"search_results\"]/tbody/tr/td/a")
+    val es = raw.iterator
+    while (es.hasNext) {
+      val e = es.next()
+      println("Establishment name: " + e.getText())
+
+      val href = e.getAttribute("href")
+      println("               URL: http://www.education.gov.uk/edubase/establishment/summary.xhtml?urn=" + findUrn(href))
+
+      def findUrn(s: String): String = {
+        val regex = ".*urn=(.*)".r
+        s match {
+          case regex(urn) => urn
+          case _ => ""
+        }
+      }
+    }
+    println("Done!")
   }
 }
